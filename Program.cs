9afa31d196c3a818c2ae67aa4337bf4be4795bf2
Brian Bocquet <brianbocquet@gmail.com>
2023-03-20 08:47:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DTPD6_Contact_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
